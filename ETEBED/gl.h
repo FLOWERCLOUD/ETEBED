70d31fa0308bbfe2a3049c3e7b703f73b10f23e1
FLOWERCLOUD <528450012@qq.com>
2017-02-22 19:57:03 +0800

    init

---
+++ ETEBED/gl.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#if defined (__APPLE__)
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#else
+#if defined _WIN64 | defined _WIN32
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#endif
+#endif
+
